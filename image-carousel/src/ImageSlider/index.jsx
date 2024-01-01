d5a7ef12d86e7cf639918dd0480a65fba9d97ca2
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-01-01 14:14:31 +0530

    wip

--- image-carousel/src/ImageSlider/index.jsx
+++ image-carousel/src/ImageSlider/index.jsx
@@ -16,10 +16,7 @@ const ImageSlider = ({ data }) => {
         <div style={{ width: '100%', height: '100%', position: 'relative' }}>
             {/* <img src={data[activeImageIndex]?.url} className='image-slider-img'></img> */}
             <div style={{ width: '100%', height: '100%', display: 'flex', overflow: 'hidden' }}>
-                {data.map(item => {
-                    return <img key={item?.url} src={item?.url} style={{translate: `${-100 * activeImageIndex}%`}} className='image-slider-img'></img>
-
-                })}
+                {data.map(item => <img key={item?.url} src={item?.url} style={{ translate: `${-100 * activeImageIndex}%` }} className='image-slider-img'></img>)}
             </div>
             <button className='carousel-button prev' onClick={onPrevClick}>
                 &#8656;
@@ -27,6 +24,11 @@ const ImageSlider = ({ data }) => {
             <button className='carousel-button next' onClick={onNextClick}>
                 &#8658;
             </button>
+            <div>
+            {data.map((_, index) => {
+                return <span key={index} onClick={() => setActiveImageIndex(index)} className={['dot', index === activeImageIndex && 'active'].join(' ')}></span>
+            })}
+            </div>
         </div>
     </>
 }
